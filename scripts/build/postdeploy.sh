d62e9601e80e66bb609862884e63e9b14f1161bc
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-06-09 19:46:15 +0200

    test

--- scripts/build/postdeploy.sh
+++ scripts/build/postdeploy.sh
@@ -5,7 +5,7 @@ set -e
 
 npx nexauth init
 # https://devcenter.heroku.com/articles/platform-api-reference#config-vars-update
-curl -n -X PATCH "https://api.heroku.com/apps/${HEROKU_APP_NAME}/config-vars" \
+curl -n -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/config-vars \
   -d '{
     "EDDSA_PRIVATE_KEY": "$(npx dotenv -p EDDSA_PRIVATE_KEY)",
     "NEXT_PUBLIC_EDDSA_PUBLIC_KEY": "$(npx dotenv -p NEXT_PUBLIC_EDDSA_PUBLIC_KEY)"
